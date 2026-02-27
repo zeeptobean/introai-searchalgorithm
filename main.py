5ccbd58e94b5a91e86215de72d26671a50b76989
vuhieu0808 <minhhieuvutran046@gmail.com>
2026-02-27 18:41:36 +0700

    feat: add aco algo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 
 import numpy as np
+from swarmalgo.aco import aco_discrete
 from util.define import *
 import function.continuous_function as contfunc
 import function.discrete_function as discfunc
@@ -14,18 +15,13 @@ from swarmalgo.abc import *
 
 # problem = contfunc.RastriginFunction(dimension=2)
 # problem = contfunc.MichalewiczFunction(dimension=2)
-# distance_matrix=np.array([[ 0, 48, 65, 68, 68],
-# [10,  0, 22, 37, 88],
-# [71, 89,  0, 13, 59],
-# [66, 40, 88,  0, 89],
-# [82, 38, 26, 78,  0]])
-# problem = discfunc.TSPFunction(distance_matrix=distance_matrix)
+distance_matrix=np.array([[ 0, 48, 65, 68, 68],
+[10,  0, 22, 37, 88],
+[71, 89,  0, 13, 59],
+[66, 40, 88,  0, 89],
+[82, 38, 26, 78,  0]])
+problem = discfunc.TSPFunction(distance_matrix=distance_matrix, dimension=5)
 # print(distance_matrix)
-# problem=discfunc.KnapsackFunction(
-#     weights=np.array([10, 20, 30, 40, 50]),
-#     values=np.array([60, 100, 120, 240, 300]),
-#     capacity=100
-# )
 adjacency_matrix = np.array([
     [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
     [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
@@ -48,17 +44,17 @@ adjacency_matrix = np.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
 ])
-problem = discfunc.KnapsackFunction(
-    weights=np.array([10, 20, 30, 40, 50]),
-    values=np.array([60, 100, 120, 240, 300]),
-    capacity=100,
-    dimension=5
-)
 # problem = discfunc.GraphColoringFunction(adjacency_matrix=adjacency_matrix, dimension=20)
+# problem = discfunc.KnapsackFunction(
+#     weights=np.array([10, 20, 30, 40, 50]),
+#     values=np.array([60, 100, 120, 240, 300]),
+#     capacity=100,
+#     dimension=5
+# )
 # result = simulated_annealing_continuous(problem, rng_seed=42, step_bound=0.5, temp=100.0, cooling_rate=0.99)
 # result = differential_evolution_continuous(problem, rng_seed=42, population_size=60, generation=1500)
 # result = differential_evolution_continuous(problem, rng_seed=42, generation=10)
-result = differential_evolution_discrete(problem, rng_seed=42, population_size=20, generation=50, mutation_factor=1, crossover_rate=0.7)
+# result = differential_evolution_discrete(problem, rng_seed=42, population_size=20, generation=50, mutation_factor=1, crossover_rate=0.7)
 # result = simulated_annealing_discrete(problem, rng_seed=42, temp=100.0, cooling_rate=0.99, step_bound=2, max_iteration=10000)
 # result = genetic_algorithm_discrete(
 #     problem, 
@@ -69,6 +65,17 @@ result = differential_evolution_discrete(problem, rng_seed=42, population_size=2
 #     crossover_rate=0.9,
 #     mutation_rate=0.2
 # )
+result = aco_discrete(
+    problem,
+    num_ants=20,
+    generation=100,
+    alpha=1.0,
+    beta=2.0,
+    evaporation_rate=0.5,
+    pheromone_deposit_weight=1.0,
+    initial_pheromone=1.0,
+    rng_seed=42
+)
 
 # print(result)
 # with open("result.dat", "wb") as f:
